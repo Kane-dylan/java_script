a3afcd204589b4971a9b3ab2adabccdd5a50ce0a
kirandas <cool.kdtech1234@gmail.com>
2024-08-10 20:32:32 +0530

    updated

---
+++ tute1/tute_1.js
@@ -0,0 +1,7 @@
+console.log(`hello`);
+console.log(`go any where else`);
+
+// window.alert(`do nothing `);
+// window.alert(`eat 5 star`);
+document.getElementById("heading").textContent = `Bla Bla Bla Bla`;
+document.getElementById(`paragraph`).textContent = `do nothing eat 5 *`;
