b3cfd45a7cf83c3f3d08ce554718de973f4eea99
kirandas <cool.kdtech1234@gmail.com>
2024-08-10 21:19:21 +0530

    updated

--- tute1/tute_1.js
+++ tute1/tute_1.js
@@ -1,14 +1,7 @@
-// console.log(`hello`);
-// console.log(`go any where else`);
+console.log(`hello`);
+console.log(`go any where else`);
 
-// // window.alert(`do nothing `);
-// // window.alert(`eat 5 star`);
-// document.getElementById("heading").textContent = `Bla Bla Bla Bla`;
-// document.getElementById(`paragraph`).textContent = `do nothing eat 5 *`;
-
-const accountId = 5541 
-let emailId = "john@lol.co.in"
-var  accountPass = "lol"
-accountCity = "delhi"
-
-console.table([accountId,emailId,accountPass,accountCity])
+// window.alert(`do nothing `);
+// window.alert(`eat 5 star`);
+document.getElementById("heading").textContent = `Bla Bla Bla Bla`;
+document.getElementById(`paragraph`).textContent = `do nothing eat 5 *`;
---
+++ tute1/tute_2.js
@@ -0,0 +1,6 @@
+const accountId = 5541 
+let emailId = "john@lol.co.in"
+var  accountPass = "lol"
+accountCity = "delhi"
+
+console.table([accountId,emailId,accountPass,accountCity])
