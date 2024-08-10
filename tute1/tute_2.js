5717bc999f89f09122b28598d7d269af82c05991
kirandas <cool.kdtech1234@gmail.com>
2024-08-11 02:26:55 +0530

    `updated`

--- tute1/tute_2.js
+++ tute1/tute_2.js
@@ -1,6 +1,16 @@
+//defining some variables by author 
 const accountId = 5541 
 let emailId = "john@lol.co.in"
 var  accountPass = "lol"
 accountCity = "delhi"
 
 console.table([accountId,emailId,accountPass,accountCity])
+
+//some changes made by author on variables
+// accountId = 1934
+emailId = "update@unknown.co.in"
+accountPass = "kook"
+accountCity = `bally`
+let accountState 
+
+console.table([accountId,emailId,accountPass,accountCity,accountState])
