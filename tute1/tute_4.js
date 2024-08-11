bf9a33adeb757164464021b841286ae45a99cd2e
kirandas <cool.kdtech1234@gmail.com>
2024-08-11 21:23:33 +0530

    11-08-24

--- tute1/tute_4.js
+++ tute1/tute_4.js
@@ -3,15 +3,39 @@
 
 // here are some examples{7 type of}
 //num, bigInt, string, null, undefined, symbol, boolean
-const point = 200
-const pointNo = 259.898
-console.log(typeof point);
+const point = 200;
+const pointNo = 259.898;
+// console.log(typeof point);
+//.............................................
 
-let shopName = "kolahal"
+let shopName = "kolahal";
+//.............................................
 
-const id = Symbol('123')
-const notId = Symbol('123')// both symbol are not same although they looks same .
-console.log(id === notId)
+const id = Symbol("123");
+const notId = Symbol("123"); // both symbol are not same although they looks same .
+// console.log(id === notId)
+//.............................................
 
-console.table(typeof[point, pointNo])
+// console.table(typeof[point, pointNo])
+
 // relational (non-primitive) datatype
+// here are some examples{3 type of}
+// array, function, object
+
+const data = ["thor", "iron man", "hulk"]
+console.log(typeof data)
+//.............................................
+let myObj = {
+  name: "kane",
+  age: 99,
+}
+console.log(typeof myObj)
+//.............................................
+const myFunction = function () {
+  console.log("prefer not to say anything !")
+}
+console.log(typeof myFunction)
+//.............................................
+let myArray = [1, "hello", true, null];
+console.log(typeof myArray)
+
