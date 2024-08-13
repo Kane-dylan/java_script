1229469ff84f4d02576b503a60e3c63b87c82070
kirandas <cool.kdtech1234@gmail.com>
2024-08-13 20:25:52 +0530

    13-08-24

--- tute1/tute_6.js
+++ tute1/tute_6.js
@@ -8,3 +8,23 @@ const lolcat = new String('not-worry')
 
 console.log(lolcat)
 console.log(lolcat.toLowerCase())
+console.log(lolcat.charAt(3))
+
+const loldog = lolcat.substring(1,6)
+console.log(loldog)
+
+console.log(lolcat.length)
+
+const doma = lolcat.slice(-9,6)
+console.log(doma)
+
+const hi = String('             hi      ')
+console.log(hi)
+console.log(hi.trim())
+
+
+const url = "https://github.com-kane/dylan"
+console.log(url.replace("-","/"))
+
+
+console.log(lolcat.split('-'))
