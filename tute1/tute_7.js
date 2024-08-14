123960ef2dc65329ba83da1c4124717c1290f62e
kirandas <cool.kdtech1234@gmail.com>
2024-08-14 21:44:25 +0530

    14-08-24

---
+++ tute1/tute_7.js
@@ -0,0 +1,21 @@
+// numbers and games
+const balance = new Number(500)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const newNum = 743.382932
+console.log(newNum.toPrecision(5))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In'))
+
+
+
+//******************Math**********************//
+
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(78.28))
+console.log(Math.min())
