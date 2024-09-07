dbbf70fb2118e7ff6370e20f318d218dfd6d8acc
kirandas <cool.kdtech1234@gmail.com>
2024-09-07 14:00:14 +0530

    07-09-24

---
+++ loops/2_while.js
@@ -0,0 +1,27 @@
+// while loop
+
+let i = 0;
+while (i<=20) {
+  // console.log(`the value of i is ${i}`)
+  i = i+2;
+}
+
+newArr = ["rat","cat","dog","fox"]
+let arr=0
+while (arr< newArr.length) {
+  // console.log(`elements are ${newArr[arr]}`);
+  arr=arr+1;
+}
+
+
+// Do While loop
+
+// do {
+//   console.log(`element is ${newArr[arr]}`)
+//   arr=arr+1
+// } while (arr<newArr);
+let game = 1
+do {
+  console.log(`no. of game is ${game}`)
+  game++
+} while (game<=10);
