3814fbd7896bc0c6c40d0be1d93987286c2b41e6
kirandas <cool.kdtech1234@gmail.com>
2024-09-09 23:08:33 +0530

    09-09-24

--- loops/5_for_each.js
+++ loops/5_for_each.js
@@ -7,3 +7,28 @@ coding.forEach((i) => {
   // console.log(i);
 });
 
+coding.forEach((i,index,arr)=> {
+  console.log(i,index,arr);
+  
+})
+
+
+const NewArr = [
+  {
+    language:"javascript",
+    extension:"js"
+  },
+  {
+    language:"python",
+    extension:"py"
+  },
+  {
+    language:"java",
+    extension:"java"
+  }
+]
+
+NewArr.forEach((item)=>{
+  console.log(item.language);
+  
+})
