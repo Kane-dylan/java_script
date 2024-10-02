489a41a333829022be1549bdb20705fb3f8b0f73
kirandas <cool.kdtech1234@gmail.com>
2024-10-02 20:05:14 +0530

    pusing js updates

--- loops/4_for_in.js
+++ loops/4_for_in.js
@@ -21,3 +21,25 @@ map.set('FR','France')
 for (const key in map) {
   console.log(key); 
 }
+
+let num = [2,5,7,9]
+let doubledNum =[]
+
+for (let i = 0; i < num.length; i++) {
+  if(num[i] === 7){
+    continue;
+  }  
+  doubledNum.push(num[i]*2);
+}
+// console.log(doubledNum);
+
+let tea =["chai","green tea","black tea","jasmine tea","herbal tea"]
+let shortTeas =[]
+
+for (const teas of tea) {
+  if(teas.length > 10){
+    break;
+  }
+  shortTeas.push(teas)
+}
+console.log(shortTeas);
